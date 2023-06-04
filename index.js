35fd61fad296da03e5beddc3909c4c21a35d3f59
venkat88676 <venkat88676@gmail.com>
2023-06-04 13:44:45 +0530

    darkMode-added

--- index.js
+++ index.js
@@ -4,4 +4,25 @@ const icon = document.querySelector(".menu-page")
       icon.classList.toggle("activate")
   }
 
+const lightmode = document.querySelector(".lightmode")
+const darkmode = document.querySelector(".darkmode")
+const body = document.body
+const nav =  document.querySelector("#nav-menu")
+const footer = document.querySelector("#contact")
+function lightMode(){
+  console.log("lightmode")
+  lightmode.classList.remove("activate")
+  darkmode.classList.add("activate")
+  nav.classList.remove("dark")
+  footer.classList.remove("dark")
+  body.classList.remove("dark")
+}
 
+function darkMode(){
+  console.log("darkmode")
+  darkmode.classList.remove("activate")
+  lightmode.classList.add("activate")
+  nav.classList.add("dark")
+  footer.classList.add("dark")
+  body.classList.add("dark")
+}
