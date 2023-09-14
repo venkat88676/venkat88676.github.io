662750fa57cfcdf392d9a6aa23441614bafd8b4d
venkat88676 <venkat88676@gmail.com>
2023-09-15 03:29:04 +0530

    updates on form

--- index.js
+++ index.js
@@ -47,3 +47,34 @@ hamburgerResume.onclick = () => {
     window.open("./resume/Venkatesh-Tiwari-Resume.pdf", "_blank")
     
 }
+
+function sendMail(e) {
+  e.preventDefault();
+  document.getElementById('sendBtn').innerHTML = `<i class="fa fa-refresh fa-spin"></i> Send`
+  document.getElementById('sendBtn').disabled = true;
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    phone:document.getElementById("phone").value,
+    message: document.getElementById("message").value,
+  };
+  console.log(params)
+
+  const serviceID = "service_yx374yb";
+  const templateID = "template_o8fbfhl";
+
+  emailjs.send(serviceID, templateID, params)
+  .then((res) => {
+    console.log(res);
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+    document.getElementById("phone").value = "";
+    alert("your message sent successfully");
+  })
+  .catch((err)=>alert(err))
+  .finally(() => {
+    document.getElementById('sendBtn').innerHTML = `Send`
+    document.getElementById('sendBtn').disabled = false;
+  })
+}
